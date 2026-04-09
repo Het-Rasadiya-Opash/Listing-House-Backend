fbf13565c0c9bda2dc12b125893c290adb13b6b4
Het <hra.opash@gmail.com>
2026-04-09 16:59:51 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { type Application } from "express";
+import cookieParser from "cookie-parser";
+import { dbConnection } from "./config/db.js";
+import errorHandler from "./middlewares/error.middleware.js";
+import userRoutes from "./routes/user.route.js";
+import listingRoutes from "./routes/listing.route.js";
+import reviewRoutes from "./routes/review.route.js";
+import bookingRoutes from "./routes/booking.route.js";
+import paymentRoutes from "./routes/payment.routes.js";
+import likeRoutes from "./routes/like.route.js";
+import cors from "cors";
+export const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.VITE_FRONTEND_BASE_URL,
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/payment", paymentRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/listing", listingRoutes);
+app.use("/api/review", reviewRoutes);
+app.use("/api/booking", bookingRoutes);
+app.use("/api/like", likeRoutes);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+  dbConnection();
+});
---
+++ src/models/user.model.ts
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface User {
+  username: string;
+  email: string;
+  password: string;
+  admin: boolean;
+}
+
+interface UserDocument extends User, mongoose.Document {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema = new mongoose.Schema<UserDocument>(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required for creating a user"],
+      trim: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Invalid Email address",
+      ],
+      unique: [true, "Email already exists."],
+    },
+    username: {
+      type: String,
+      required: [true, "Name is required for creating a user"],
+      trim: true,
+      unique: [true, "Username already exists."],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      validate: {
+        validator: function (v: string) {
+          return /^(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{6,}$/.test(v);
+        },
+        message: "Password must contain at least 1 capital letter and 1 special character",
+      },
+      select: false,
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  return;
+});
+
+userSchema.methods.comparePassword = async function (password: string) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const userModel = mongoose.model<UserDocument>("User", userSchema);
